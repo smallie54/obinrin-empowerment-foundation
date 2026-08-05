780f990823c9325a0af03a7937dc25c792a3129d
Smallie54 <adefagbayipraise@gmail.com>
2026-08-05 21:42:55 +0100

    update cors

--- obinrin-backend/src/server.js
+++ obinrin-backend/src/server.js
@@ -61,18 +61,6 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
 
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 
