fc241eaaaf404406239f830325b44fcb41d59e84
Smallie54 <adefagbayipraise@gmail.com>
2026-08-05 21:54:15 +0100

    changed publicApi

--- obinrin-foundation-js/src/lib/public.js
+++ obinrin-foundation-js/src/lib/public.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const publicApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
