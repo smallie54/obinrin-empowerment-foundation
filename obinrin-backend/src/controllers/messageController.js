277d51a7a327ada937558f0fc6c0efa62a3024df
Smallie54 <adefagbayipraise@gmail.com>
2026-08-11 22:22:08 +0100

    fix message controller

--- obinrin-backend/src/controllers/messageController.js
+++ obinrin-backend/src/controllers/messageController.js
@@ -1,6 +1,6 @@
 // controllers/messageController.js
 import Message from "../models/message.js";
-import { generateThankYouDraft } from "../config/geminiService.js";
+// import { generateThankYouDraft } from "../config/geminiService.js";
 import { generateThankYouDraft } from "../config/groqServices.js";
 import { sendEmail } from "../config/mailer.js";
 
